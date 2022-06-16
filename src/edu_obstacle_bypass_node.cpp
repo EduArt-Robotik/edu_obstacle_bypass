ed9b9942c6b984cd0e80461f558cddcb112d280d
Antonello Pastore <antonello.pastore@yahoo.de>
2022-06-16 16:44:43 +0200

    Added Changes from first testing

--- src/edu_obstacle_bypass_node.cpp
+++ src/edu_obstacle_bypass_node.cpp
@@ -18,9 +18,9 @@ ros::Subscriber scan_sub;   // Initialize the Subscriber
 void scanCallback(const sensor_msgs::LaserScanConstPtr& scan_msg)
 {
   std::vector<float> scan = scan_msg->ranges;   // Collecting the meassured ranges from the arriving scan data
-  geometry_msgs::Twist msg;                   // Initialization of the turning angle
+  geometry_msgs::Twist msg;                     // Initialization of the turning angle
 
-  int front_index = 1;          // Initialize the Index of the measurement in front of the robot
+  int front_index = 1;                          // Initialize the Index of the measurement in front of the robot
   float front_range = 0.5;                      // Initialize the maximum range [m] before the obstacle for triggering the manoeuvre
   
   /**
@@ -31,10 +31,10 @@ void scanCallback(const sensor_msgs::LaserScanConstPtr& scan_msg)
 
   if (scan[front_index] < front_range)
   {
-    int dir = (rand() % 2 == 0) ? 1 : -1;                               // Initialize the direction of the manoeuvre
-    float ang_vel = 0.3 * dir;                                          // Initialize the angular velocity [rad/s]
-    float target_angle = (((double) rand() / (RAND_MAX)) * M_PI_2 + M_PI_2) * dir;   // Initialize the target angle [rad]
-    float time = target_angle / ang_vel;                            // Initialize the publishing time [s]
+    int dir = 1;                                                      // Initialize the direction of the manoeuvre
+    float ang_vel = 0.3 * dir;                                        // Initialize the angular velocity [rad/s]
+    float target_angle = (((double) rand() / (RAND_MAX)) * M_PI_2);   // Initialize the target angle [rad]
+    float time = target_angle / ang_vel;                              // Initialize the publishing time [s]
     std::cout << "pub_time: " << pub_time << std::endl;
     std::cout << "scan[front_index]: " << scan[front_index] << std::endl;
 
@@ -71,14 +71,14 @@ int main(int argc, char **argv)
   * publish on a given topic name: the generated twist-msg to move your robot.
   * Tell the master that we are going to be publishing every 10ms the message of type geometry_msgs/Twist on the topic /cmd_vel.
   */
-  vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+  vel_pub = n.advertise<geometry_msgs::Twist>("/vel/teleop", 1);
 
   /** SUBSCRIBER
   * The subscribe() call is how you tell ROS that you want to receive messages
   * on a given topic.  Messages are passed to a callback function, here
   * called  scanCallback to determine the target angle.
   */
-  scan_sub = n.subscribe("safee/lidar_scan", 1000, scanCallback);
+  scan_sub = n.subscribe("front/scan", 1000, scanCallback);
 
   // Calling the Callbacks (once)
   ros::spin();
